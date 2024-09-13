ad057dde8e958e73305a4cacea67f484162616a3
Rohit Sahoo <rohitsahoo112003@gmail.com>
2024-09-13 23:01:35 +0530

    FrontEnd work

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-
+import { createBrowserRouter, RouterProvider} from "react-router-dom"
 import './App.css';
 
 function App() {
+
+  const route = createBrowserRouter([
+    {
+      path: '/',
+      element: 'Home page'
+    },
+    {
+      path: '/add',
+      element: 'Add user page'
+    },
+    {
+      path: '/edit',
+      element: 'Update user page'
+    },
+  ])
+
   return (
     <div className="App">
-      <h1>Hello CRUD App</h1>
+      <RouterProvider router={route}></RouterProvider>
     </div>
   );
 }
